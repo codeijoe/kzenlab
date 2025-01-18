5917a267087ac63daff24180d26f04027cc66e24
Indra Wahjoedi <indra.wahjoedi@gmail.com>
2025-01-18 16:31:52 +0700

    udisks2

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,7 +6,7 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle_vbox_2016.gpg] http:/
 
 sudo apt update
 sudo apt install -yqq cmake
-sudo apt install -yqq udisksctl
+sudo apt install -yqq udisks2
 sudo apt install -yqq debootstrap
 sudo apt install -yqq virtualbox-7.1
 sudo apt install -yqq qemu-system
